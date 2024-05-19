7f2b8396fbf316f737d3537670bd6c799100218b
Booze33 <tebetisloh@gmail.com>
2024-05-20 00:06:30 +0200

    added right nav component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Boss' };
+  const loggedIn = { firstName: 'Boss', lastName: 'Sui', email: 'sui@gmail.com' };
 
   return (
     <section className="home">
@@ -22,7 +23,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSCATIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
     </section>
   )
 }
