2e5a444fd4ce42ed4b1bd539c450f5f499cda51e
Booze33 <tebetisloh@gmail.com>
2024-06-12 17:19:09 +0200

    Recent transaction

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import React from 'react'
+import { BankTabItem } from './BankTabItem'
+import BankInfo from './BankInfo'
 
 const RecentTransactions = ({
   accounts,
@@ -21,11 +23,30 @@ const RecentTransactions = ({
 
       <Tabs defaultValue={appwriteItemId} className="w-full">
         <TabsList className="recent-transactions-tablist">
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+          {accounts.map((account: Account) => (
+            <TabsTrigger key={account.id} value={account.appwriteItemId}>
+              <BankTabItem
+                key={account.id}
+                account={account}
+                appwriteItemId={appwriteItemId}
+              />
+            </TabsTrigger>
+          ))}
         </TabsList>
-        <TabsContent value="account">Make changes to your account here.</TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
+
+        {accounts.map((account: Account) => (
+          <TabsContent
+            value={account.appwriteItemId}
+            key={account.id}
+            className="space-y-4"
+          >
+            <BankInfo
+              account={account}
+              appwriteItemId={appwriteItemId}
+              type="full"
+            />
+          </TabsContent>
+        ))}
       </Tabs>
 
     </section>
