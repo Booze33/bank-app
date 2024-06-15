2d7b2e4c80245921ef2f06969af1ea2e8488097d
Booze33 <tebetisloh@gmail.com>
2024-06-15 13:53:58 +0200

    Hanndle user tranactions

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,18 @@
 import HeaderBox from '@/components/HeaderBox';
+import PaymentTransferForm from '@/components/PaymentTransferForm';
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.action';
 import React from 'react'
 
-const Transfer = () => {
+const Transfer = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id
+  })
+
+  if(!accounts) return;
+
+  const accountsData = accounts?.data;
   return (
     <section className="payment-transfer">
       <HeaderBox
@@ -10,7 +21,9 @@ const Transfer = () => {
       />
 
       <section className="size-full pt-5">
-        
+        <PaymentTransferForm
+          accounts={accountsData}
+        />
       </section>
     </section>
   )
