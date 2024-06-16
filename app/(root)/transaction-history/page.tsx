e525c08b04bccee1c589db602ed0f9145db2266c
Booze33 <tebetisloh@gmail.com>
2024-06-16 14:06:24 +0200

    Pagination and Spending categories

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox';
+import { Pagination } from '@/components/Pagination';
 import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.action';
@@ -18,6 +19,14 @@ const TransactionHistory = async ({ searchParams: {id, page }}: SearchParamProps
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
   const account = await getAccount({ appwriteItemId });
+
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
+
+  const indexOfLastTransaction = currentPage * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = account?.transactions.slice(indexOfFirstTransaction, indexOfLastTransaction); 
 
   return (
     <section className="transactions">
